63a5002c86b434763719f9e108c23a7b51f950da
Anderson Jose Simplicio <anderson25eu@gmail.com>
2024-01-06 05:28:16 -0300

    Ajuste Tela Detalhes

--- projeto/apps/receitas/urls.py
+++ projeto/apps/receitas/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .import views 
 
 
 urlpatterns = [
-    path('',home,name='home'),
+    path('',views.home,name='home'),
+    path('receitas/<int:id>/details',views.recietas,name='receita'),
 ]
--- projeto/apps/receitas/views.py
+++ projeto/apps/receitas/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from utils.receitas.factory import make_produto
 
 
 def home(request):
-    return render(request,'home/home.html',context={'name':'Anderson Simplicio','title':'HOME'},)
+    return render(request,'home/home.html',context={
+        'name':'Anderson Simplicio',
+        'produtos': [make_produto() for _ in range(10)],
+        },)
+
+
+def recietas(request,id:int):
+    return render(request,'home/receita.html',
+                  context={'produto': make_produto(),
+                           'is_detalis':True
+                           })
