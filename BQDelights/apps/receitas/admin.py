df5ba258db9112d0f1e64777d63627f08a63c81d
Anderson Jose Simplicio <anderson25eu@gmail.com>
2024-01-11 04:21:52 -0300

    Criando Test Automático

--- BQDelights/apps/receitas/admin.py
+++ BQDelights/apps/receitas/admin.py
@@ -8,9 +8,9 @@ class CategoriaAdmin(admin.ModelAdmin):
 
 @admin.register(Receita)
 class ReceitasAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'is_published','category','author','preparation_steps_is_html')
-    list_filter = ('is_published',)
-    search_fields = ('title',) 
+    list_display = ('title', 'created_at', 'is_published', 'category','author','preparation_steps_is_html')
+    list_filter = ('is_published', 'category')
+    search_fields = ('title', 'category__nome') 
     list_editable = ('is_published',)
 
 
