49900737dbba306de4ecaf847d5718c0415b335d
Anderson Jose Simplicio <anderson25eu@gmail.com>
2024-01-24 12:14:06 -0300

    Ajuste de Configuração pytest-django

--- BQDelights/apps/receitas/tests.py
+++ BQDelights/apps/receitas/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
+from django.conf import settings
 
-class ReceitaURLsTest(TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-       # Clean up run after every test method.
-       pass
+settings.configure(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
+    },
+    # Adicione outras configurações necessárias aqui
+)
+class RecipeURLsTest(TestCase):
     def test_the_pytest_is_ok(self):
-        assert 1 == 2, 'Um é igual a um'
+        assert 1 == 1, 'Um é igual a um'
